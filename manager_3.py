777ec0cbf20a118c1a6eb0135931d74e6888df59
Ayo <ayo.obembe@gmail.com>
2015-11-25 15:06:11 +0000

    redoing/reading through manager

---
+++ manager_3.py
@@ -0,0 +1,40 @@
+"""betfair bot manager"""
+from time import sleep
+import traceback
+from sys import argv, exit
+from login_config import login_info
+
+#User Info
+USERNAME = login_info['username']
+PASSWORD = login_info['password']
+APP_KEY  = login_info['app_key_live']
+
+#Exchange Info
+#Launch bot using 'python gubber_ng/manager.py --aus' to use Australian xchnge
+AUS = False            #default to UK exchange
+if '--aus' in argv:
+    AUS = True
+EXIT_ON_ERROR = True   #set to False when bot is ready to run 24/7
+
+
+#Run Operation
+while True:            #Loop forever
+    try:
+        from gubbins_ng import BetBot
+        from logger import Logger
+        log = Logger(AUS)
+        log.xprint('STARTING BOT ...')
+        #start bot
+        bot = BetBot()
+        bot.run(USERNAME, PASSWORD, APP_KEY, AUS)
+    except Exception as exc:
+        from logger import Logger
+        Log = Logger()
+        msg = traceback.format_exc()
+        http_err = 'ConnectionError:'
+        if http_err in msg:
+            msg = '%s%s' % (http_err, msg.rpartition(http_err)[2])
+        msg = '#### BOT CRASH ####\n%s' % msg
+        log.xprint(msg, err = True)
+        if EXIT_ON_ERROR: exit()
+    sleep(60)
