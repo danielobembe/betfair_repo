434114967e5385b67931b7942a3d72f0aa05c6fc
Ayo <ayo.obembe@gmail.com>
2015-11-25 12:20:25 +0000

    retyping logger.py

---
+++ logger_2.py
@@ -0,0 +1,48 @@
+import os
+from datetime import datetime
+
+class Logger(object):
+    """print info to terminal and save to log file"""
+
+    def __init__(self, aus=False):
+        self.prefix = '[UK]'
+        if aus: self.prefix = '[AUS]'
+        self.abs_path = os.path.abspath(os.path.dirname(__file__))
+        self.log_path = self.abs_path + '/log.txt'
+        self.err_path = self.abs_path + '/err_log.txt'
+        self.max_logs = 5000 #maximum data entries
+        self.bot_version = None #set externally
+
+        def xprint(self, data='', err=False):
+            if data:
+                #set file path
+                fpath = self.log_path
+                if err: fpath = self.err_path
+                #build data string
+                data = str(data) #belt n' braces???
+                str_date = datetime.utcnow().strftime('%d/%m/%Y %H:%M:%S.%f GMT')
+                div = '%s\n' % ('-' * 40)
+                bot_version = 'Bot version: %s' % self.bot_version
+                temp = '%s %s\n' % (self.prefix, str_date)
+                temp += '%s\n' % bot_version
+                temp += '%s\n' % data
+                temp += div
+                #check if log file exists
+                if os.path.exits(fpath):
+                    #read file + append new data
+                    fd = open(fpath, 'r').read() + temp
+                    #check data length
+                    while fd.count(div) > self.max_logs:
+                        #remove first (oldest) entry
+                        fd = fd.partition(div)[2]
+                    #write a new file to data
+                    open(fpath, 'w').write(fd)
+                else:
+                    open(fpath, 'w').write(temp)
+                #print to terminal
+                try: #req'd for when remote terminal is closed 
+                    print(temp)
+                except:
+                    pass
+                
+        
